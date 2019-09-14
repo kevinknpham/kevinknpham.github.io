751210cb79b2031e811870739b1d28d3865462c4
Kevin <kevinknpham@gmail.com>
2019-09-13 17:51:40 -0700

    finished up skills section and decided on a structure for my js

---
+++ data/skills.js
@@ -0,0 +1,36 @@
+/*
+Kevin Pham
+This file contains data used to populate the skills section of index.html.
+*/
+
+const SKILLS = [
+    {
+        "title": "Java",
+        "description": "Java was the first programming language I learned and is the one most used in my classes.",
+        "image": "images/java.png"
+    },
+
+    {
+        "title": "HTML/CSS",
+        "description": "I've been a teaching assistant for CSE 154, a web development course at UW.  In addition, I've made several websites in my free time which gave me a lot of practice with these languages.",
+        "image": "images/htmlcss.png"
+    },
+
+    {
+        "title": "JavaScript",
+        "description": "I learned JavaScript for web development and have taught it in class.  It is a powerful language that I've used in nearly all of my personal projects.",
+        "image": "images/js.png"
+    },
+
+    {
+        "title": "PHP",
+        "description": "PHP is the language I used to first learn server side development.  I have used this lightweight language to create several public APIs for websites and apps that require a server.",
+        "image": "images/php.png"
+    },
+
+    {
+        "title": "MySQL",
+        "description": "I learned and taught the basics of MySQL.  In addition, I've managed a small database using MySQL",
+        "image": "images/mysql.svg"
+    }
+];
---
+++ index.js
@@ -0,0 +1,69 @@
+/*
+Kevin Pham
+JS file used to populate index.html.
+*/
+
+(function() {
+    "use strict";
+
+
+
+    window.addEventListener("load", init);
+
+    /**
+     * Function that populates the page when it is loaded
+     */
+    function init() {
+        loadCards("skill-card", id("skills-container"), SKILLS);
+    }
+
+    /**
+     * Loads cards into the given parent element.
+     * @param {String} type - class to add to card
+     * @param {Object} parent - DOM for container to hold the cards
+     * @param {Object[]} data - Array where each element is an object with the name, description, and image for each card
+     */
+    function loadCards(type, parent, data) {
+        for (let i = 0; i < data.length; i++) {
+            console.table(data[i]);
+            parent.appendChild(generateCard(data[i], type));
+        }
+    }
+
+    /**
+     * Generates a single card's DOM for the skills section
+     * @param {String} title - title for card
+     * @param {String} description - description for paragraph on card
+     * @param {String} image - url for img of card
+     * @return {Object} - DOM for given data
+     */
+    function generateCard({title, description, image}, type) {
+        let result = ce("div");
+        result.classList.add("card");
+
+        let img = ce("img");
+        img.src = image;
+        img.alt = title;
+        result.appendChild(img);
+
+        let desc = ce("div");
+        desc.classList.add("description");
+
+        let h3 = ce("h3");
+        h3.innerText = title;
+        desc.appendChild(h3);
+
+        let p = ce("p");
+        p.innerText = description;
+        desc.appendChild(p);
+
+        result.appendChild(desc);
+        return result;
+    }
+
+    /*==========Helper Functions==========*/
+    const ce = (el) => document.createElement(el);
+    const id = (id) => document.getElementById(id);
+    const qs = (query) => document.querySelector(query);
+    const qsa = (query) => document.querySelectorAll(query);
+})();
